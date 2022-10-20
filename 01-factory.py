6acb8861e95685bc45b6bead76d56ac595d910f7
Raul Juarez <rauljrz@linux-dev.fibertel.com.ar>
2022-10-20 18:03:26 -0300

    fix(pep8): para normalizar con respecto al pep8.

--- 01-factory.py
+++ 01-factory.py
@@ -17,15 +17,16 @@ class Cat:
     def speak(self):
         return "Meow!"
 
+
 def get_pet(pet="dog"):
     """ The Factory Method """
 
     pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
     return pets[pet]
 
+
 d = get_pet("dog")
 print(d.speak())
 
 c = get_pet("cat")
 print(c.speak())
-
